ec05d47b248137de74aaa1af8a79e7e87782abcb
Nam <namthse03439@fpt.edu.vn>
2016-07-22 14:02:25 +0800

    commit latest

--- app/src/main/java/com/android/msahakyan/expandablenavigationdrawer/Fragment/TimeTableFragment.java
+++ app/src/main/java/com/android/msahakyan/expandablenavigationdrawer/Fragment/TimeTableFragment.java
@@ -30,7 +30,6 @@ import com.android.msahakyan.expandablenavigationdrawer.BaseClass.StringClient;
 import com.android.msahakyan.expandablenavigationdrawer.Preferences;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -50,8 +49,7 @@ import retrofit2.Response;
  * create an instance of this fragment.
  */
 public class TimeTableFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
